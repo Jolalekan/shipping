d7edff8f4b040f89ad18a6fd007c3d67ed8796ed
jolalekan <lekimite@gmail.com>
2023-12-14 09:58:45 +0100

    update on the footer and tacking

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -152,7 +152,7 @@ export const Footer = () => {
                                 <path
                                     d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                             </svg>
-                            <p className="mb-4 justify-center items-center md:justify-start "> info@mail.com
+                            <p className="mb-4 justify-center items-center md:justify-start "> lekimite@gmail.com
                             </p>
 
                         </div>
--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -47,20 +47,20 @@ const Tracking = () => {
             <Company />
 
             <div className='max-w-2xl mx-auto p-6'>
-                <h2 className='text-gray-600 text-center text-xl font-bold underline lg:text-2xl'>Track Your Parcel</h2>
+                <h2 className='text-gray-600 text-center text-xl font-bold underline md:text-3xl'>Track Your Parcel</h2>
                 <form className='flex flex-col gap-5 m-20' onSubmit={handleSubmit}>
-                    <h2 className='text-center text-x md:text-2xl font-medium'>Tracking Number</h2>
+                    <h2 className='text-center text-xl md:text-2xl font-medium'>Enter Tracking Number</h2>
                     <input type="text"
-                        placeholder='Enter Tracking Number'
+                        placeholder='Tracking Number'
                         value={trackingNumber}
                         onChange={(e) => setTrackingNumber(e.target.value)}
-                        className="w-48 lg:w-96 self-center"
+                        className="text-center w-48 md:w-96 self-center  "
                     />
                     <button className='self-center rounded-xl w-32 md:w-48 bg-gray-900 p-4 text-gray-200 font-medium disabled:opacity-80' disabled={loading}>{loading ? "Tracking..." : "Track"}</button>
                     {error && <p className='text-red-900 text-sm font-medium'>{error}</p>}
                 </form>
             </div>
-            <Partner/>
+            <Partner />
         </div>
     )
 }
